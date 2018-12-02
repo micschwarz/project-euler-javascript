661e165cbf93596444a4a39c87c8d0f05592d9ba
Michael Schwarz <089michael@gmail.com>
2018-12-02 15:42:05 +0100

    Add solution for Problem 1

---
+++ src/1/index.js
@@ -0,0 +1,33 @@
+/**
+ * Calculates the sum of all the multiples of 3 or 5 below n
+ * @param {number} max - Max. multiplier (not included)
+ * @return {number} sum of all the multiples of 3 or 5 below n
+ */
+function Problem1(max) {
+    let sum = 0;
+
+    for (let i = 1; i < max/3; i++) {
+        sum += i*3;
+        if (isMultipleOf5Allowed(i*5, max-1)) {
+            sum += i*5;
+        }
+    }
+
+    return sum;
+}
+
+/**
+ * Check if multiple of 5 is allowed to be added to sum
+ * @param {number} multiplication - multiple of 5
+ * @param {number} max - Max. allowed multiplier
+ * @returns {boolean} isMultipleOf5Allowed
+ */
+function isMultipleOf5Allowed(multiplication, max) {
+    return multiplication < max && multiplication % 15 !== 0;
+}
+
+const time = process.hrtime();
+const solution = Problem1(1000);
+const duration = process.hrtime(time);
+
+console.log(`Solution ${solution} found in ${duration[0]}s and ${duration[1]}ns`);
